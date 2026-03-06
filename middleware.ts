6d61cc4ee1e3a43db1f88877505f797e56fa30b8
Harshp61 <pandeyh477@gmail.com>
2026-03-06 22:44:35 +0530

    setup supabase and middleware for the auth purpose

---
+++ middleware.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createServerClient } from "@supabase/ssr";
+
+export async function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            response.cookies.set(name, value, options)
+          );
+        },
+      },
+    }
+  );
+
+  const { data } = await supabase.auth.getUser();
+
+  if (!data.user) {
+    return NextResponse.redirect(
+      new URL(
+        "/login?error=Please login first to access this route.",
+        request.url
+      )
+    );
+  }
+
+  return response;
+}
+
+export const config = {
+  matcher: [],
+};
