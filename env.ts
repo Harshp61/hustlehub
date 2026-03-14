59c9b287b263bf81c95f96464c690805c132d306
Harshp61 <pandeyh477@gmail.com>
2026-03-15 01:18:46 +0530

    ..

--- env.ts
+++ env.ts
@@ -1,5 +1,5 @@
 class Env {
-  static S3_BUCKET: string = process.env.NEXT_PUBLIC_BUCKET!;
+  static S3_BUCKET: string = process.env.NEXT_PUBLIC_S3_BUCKET!;
   static SUPABASE_URL: string = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 }
 
