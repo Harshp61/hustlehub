92cd56960079bb98410987a972026559f907f301
Harshp61 <pandeyh477@gmail.com>
2026-03-15 01:23:25 +0530

    ...

--- app/(front)/user/[id]/page.tsx
+++ app/(front)/user/[id]/page.tsx
@@ -1,24 +1,25 @@
 import React from "react";
 import { createClient } from "@/lib/supabase/supabaseServer";
-import { cookies } from "next/headers";
 import UserAvatar from "@/components/common/UserAvatar";
 import { getS3Url } from "@/lib/helper";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import PostCard from "@/components/post/PostCard";
 import CommentCard from "@/components/comments/CommentCard";
-export default async function User({ params }: { params: { id: string } }) {
+export default async function User({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
   const supabase = await createClient();
+  const { data: loggedInUser } = await supabase.auth.getUser();
   const { data: user } = await supabase
     .from("users")
     .select("id,name,username,email,profile_image,metadata")
-    .eq("id", params?.id)
+    .eq("id", id)
     .single();
   const { data: posts, error } = await supabase
     .rpc("get_posts_with_likes", {
-      request_user_id: "aa6bfaf8-1ecf-48be-b049-09aed6bb9070",
+      request_user_id: loggedInUser?.user?.id ?? id,
     })
     .order("post_id", { ascending: false })
-    .eq("user_id", params?.id);
+    .eq("user_id", id);
 
   // * Fetch comments
   const { data: comments } = await supabase
@@ -26,18 +27,22 @@ export default async function User({ params }: { params: { id: string } }) {
     .select(
       "id ,image,content,created_at ,users(id,name,username,profile_image)"
     )
-    .eq("user_id", params?.id);
+    .eq("user_id", id);
   return (
     <div>
       <div className="flex justify-between items-center">
         <div>
           <p className="text-2xl font-bold">{user?.name}</p>
-          <p className="font-bold">@{user?.metadata?.["username"]}</p>
+          <p className="font-bold">@{user?.username}</p>
         </div>
         <UserAvatar
           name={user?.name}
           image={
-            user?.profile_image != null ? getS3Url(user?.profile_image) : ""
+            user?.profile_image
+              ? user.profile_image.startsWith("http")
+                ? user.profile_image
+                : getS3Url(user.profile_image)
+              : ""
           }
           height={5}
           width={5}
